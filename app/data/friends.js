49f2e8bb48aff9dc1842245f8c69dfb7a7265f4a
Rachel Puder <rcompuder@gmail.com>
2019-11-29 11:38:28 -0500

    fixed again

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,22 +3,22 @@ var friendsList = [
     {
       name: "Cardi B",
       photo: "https://vengeancemag.com/wp-content/uploads/2018/01/cardib_zpsbnheoxld.jpg",
-      scores: [1,3,3,4,5,6,7,9,10,11],
+      scores: [1,3,3,4,5,4,2,3,1,1],
     },
     {
       name: "Offset",
       photo: "https://techcrunch.com/wp-content/uploads/2019/10/Offset_Enviro_0300.jpg?w=1390&crop=1",
-      scores: [2,3,4,5,6,7,8,9,10,11],
+      scores: [2,3,2,3,3,3,5,2,1,1],
     },
     {
       name: "Kulture",
       photo: "https://peopledotcom.files.wordpress.com/2018/12/kulture-cardi-b.jpg?w=768",
-      scores: [3,3,5,6,6,7,9,10,11,11],
+      scores: [3,3,5,3,3,1,5,1,2,4],
     },
     {
       name: "Henny",
       photo: "https://c8d8q6i8.stackpathcdn.com/wp-content/uploads/2018/05/Hennessy-Carolina-Contact-Information.jpg",
-      scores: [3,5,6,7,6,9,9,11,11,13],
+      scores: [3,5,2,4,5,4,5,2,3,4],
     }
   ];
   
