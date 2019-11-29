ea989e18de6b2e58d0e02ae5d46b9e7ad005cb63
Rachel Puder <rcompuder@gmail.com>
2019-11-29 11:20:56 -0500

    server.js created

---
+++ app/server.js
@@ -0,0 +1,27 @@
+
+//dependencies
+var express = require("express");
+
+//express config
+
+
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets port
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Router
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//listenter
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
