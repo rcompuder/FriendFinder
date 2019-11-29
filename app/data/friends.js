d08d637b2ea77fa4bd7bc2158e3764d7b6af7025
Rachel Puder <rcompuder@gmail.com>
2019-11-29 11:30:38 -0500

    friendList created

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+
+var friendsList = [
+    {
+      name: "Cardi B",
+      photo: "https://vengeancemag.com/wp-content/uploads/2018/01/cardib_zpsbnheoxld.jpg",
+      scores: [1,2,3,4,5,6,7,8,9,10],
+    },
+    {
+      name: "Offset",
+      photo: "https://techcrunch.com/wp-content/uploads/2019/10/Offset_Enviro_0300.jpg?w=1390&crop=1",
+      scores: [2,3,4,5,6,7,8,9,10,11],
+    },
+    {
+      name: "Kulture",
+      photo: "https://peopledotcom.files.wordpress.com/2018/12/kulture-cardi-b.jpg?w=768",
+      scores: [3,4,5,6,7,8,9,10,11,12],
+    },
+    {
+      name: "Henny",
+      photo: "https://c8d8q6i8.stackpathcdn.com/wp-content/uploads/2018/05/Hennessy-Carolina-Contact-Information.jpg",
+      scores: [4,5,6,7,8,9,10,11,12,13],
+    }
+  ];
+  
+  //  export the array
+  module.exports = friendsList;
